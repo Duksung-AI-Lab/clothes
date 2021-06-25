1231c1639d023a9e0223bad5a20def4e6f28b03d
DESKTOP-V5AI4ME\awe08 <awe080798@gmail.com>
2021-06-25 15:52:38 +0900

    edit the collar_cnn.py
    edit CNN model
    add more layers and graph only accuracy
    edit imageGenerators

--- collars_cnn.py
+++ collars_cnn.py
@@ -1,120 +1,125 @@
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
 import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping
 
-# 랜덤시드 고정시키기
-#np.random.seed(4)
+import tensorflow.keras.models as models
+from tensorflow.keras import layers
+from tensorflow.python.keras.layers import Dropout
 
-train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
+import matplotlib.pyplot as plt
 
-train_generator = train_datagen.flow_from_directory(
-        '/gdrive/My Drive/Colab Notebooks/dataset/shirts_collar/train_shirts',
-        shuffle=True,
-        target_size=(32, 20),
-        batch_size=5,
-        class_mode='categorical')
+import numpy as np
+import tensorflow.compat.v1 as tf
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+tf.disable_v2_behavior()
+# import tensorflow as tf
 
-test_generator = test_datagen.flow_from_directory(
-        '/gdrive/My Drive/Colab Notebooks/dataset/shirts_collar/test',
-        shuffle = True,
-        target_size=(32, 20),
-        batch_size=5,
-        class_mode='categorical')
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # 텐서플로가 두 번째 GPU만 사용하도록 제한
+    try:
+        tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
+    except RuntimeError as e:
+        # 프로그램 시작시에 접근 가능한 장치가 설정되어야만 합니다
+        print(e)
 
-val_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
+seed = 0
+np.random.seed(seed)
+tf.set_random_seed(seed)
+# tf.random.set_seed(seed)
 
-val_generator = val_datagen.flow_from_directory(
-        '/gdrive/My Drive/Colab Notebooks/dataset/shirts_collar/train_shirts',
-        shuffle=True,
-        target_size=(32, 20),
-        batch_size=5,
-        class_mode='categorical')
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
-# predict_datagen = ImageDataGenerator(rescale=1./255)
+rootPath = 'dataset/catAdog'
+img_size = (125, 150)
 
-# predict_generator = test_datagen.flow_from_directory(
-#         '/gdrive/My Drive/Colab Notebooks/dataset/shirts_collar/predict',
-#         target_size=(32, 20),
-#         batch_size=3,
-#         class_mode='categorical')
+imageGenerator = ImageDataGenerator(
+    rescale=1. / 255,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    brightness_range=[.2, .2],
+    horizontal_flip=True,
+)
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=(32,20,3)))
-model.add(MaxPooling2D(pool_size=(2, 2)))#
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+trainGen = imageGenerator.flow_from_directory(os.path.join(rootPath, 'train'), class_mode='categorical',
+                                              target_size=img_size, shuffle=True,
+                                              color_mode='rgba')
 
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))#
-model.add(Dense(128, activation='relu'))#
+valGenerator = ImageDataGenerator(rescale=1. / 255)
+
+valGen = valGenerator.flow_from_directory(os.path.join(rootPath, 'val'), class_mode='categorical',
+                                          target_size=img_size, shuffle=True,
+                                          color_mode='rgba')
+
+testGenerator = ImageDataGenerator(rescale=1. / 255)
+
+testGen = testGenerator.flow_from_directory(os.path.join(rootPath, 'test'), class_mode='categorical',
+                                            target_size=img_size, shuffle=True,
+                                            color_mode='rgba')
+
+# x, y = trainGen.next()
+# print(x[0].shape)
+# plt.imshow(x[0])
+# plt.show()
+
+model = models.Sequential()
+
+model.add(layers.InputLayer(input_shape=(img_size + (4,))))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(3, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#model.compile(loss='hinge', optimizer='adam', metrics=['accuracy'])
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
 
-# MODEL_DIR = '/gdrive/My Drive/Colab Notebooks/models/'
-# if not os.path.exists(MODEL_DIR):
-#   os.mkdir(MODEL_DIR)
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
 
-# modelpath="/gdrive/My Drive/Colab Notebooks/models/{epoch:02d}-{val_loss:.4f}.hdf5"
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(2, activation='softmax'))
 
-# checkpointer = ModelCheckpoint(filepath=modelpath,monitor='val_loss',verbose=1)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
-# history=model.fit_generator(
-#         train_generator,
-#         steps_per_epoch=train_generator.samples/3,
-#         epochs=30,
-#         validation_data=val_generator,
-#         validation_steps=3,
-#         callbacks=[checkpointer])
+epochs = 30
+history = model.fit_generator(trainGen,
+                              epochs=epochs,
+                              steps_per_epoch=trainGen.samples / epochs * 5,
+                              validation_data=valGen,
+                              validation_steps=valGen.samples / epochs,
+                              callbacks=[early_stopping])
 
-history=model.fit_generator(
-        train_generator,
-        steps_per_epoch=train_generator.samples/5,
-        epochs=30,
-        validation_data=val_generator,
-        validation_steps=10)
+print("\n Valid Accuracy: %.4f" % (model.evaluate_generator(valGen)[1]))
+print("\n Test Accuracy: %.4f" % (model.evaluate_generator(testGen)[1]))
 
-print("-- Evaluate --")
-scores = model.evaluate_generator(test_generator, steps=5)
-print("%s: %.2f%%" %(model.metrics_names[1], scores[1]*100))
+# graph
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-model.save('/gdrive/My Drive/Colab Notebooks/dataset/shirts_collar/collar_type_ff.h5')
+epochs = range(len(acc))
 
-# print("-- Predict --")
-# output = model.predict_generator(predict_generator, steps=5)
-# np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
-# print(test_generator.class_indices)
-# print(output)
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
 
-print(history.history)
+plt.figure()
 
-#훈련 과정 시각화 (정확도)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Full Image Model accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend(['Train', 'Validation'], loc='upper left')
+plt.plot(epochs, loss, 'go', label='Training Loss')
+plt.plot(epochs, val_loss, 'g', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
 plt.show()
 
-#훈련 과정 시각화 (손실)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.show()
+model.save('collars_model.h5')
