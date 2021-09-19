cd6794c83f44b6f757711f92cb95961adc04dbef
kri628 <00a3d2@duksung.ac.kr>
2021-09-20 01:38:02 +0900

    image table 구현

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,4 @@
+import glob
 import os, sys
 
 real_path = os.path.dirname(os.path.realpath(__file__))
@@ -25,27 +26,30 @@ def clothes():
     return render_template('clothes.html')
 
 
-result_img_path, user_img_path = None, None
+class_file_list, search_file_list = None, None
+app.config['IMG_FOLDER'] = os.path.join('static', 'images/result_img')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def post():
     if request.method == 'POST':
         # root_path()
-        global result_img_path, user_img_path
+        global class_file_list, search_file_list
         if "classform" in request.form:
             # Reference Image
             collar = request.form['collars']
             pattern = request.form['pattern']
-            result_dir_path = './images/result_img/' + str(collar) + '_' + str(pattern)
-            result_img_path = result_dir_path + '/' + 'result3.jpg'
+            result_dir_path = os.path.join(app.config['IMG_FOLDER'], str(collar) + '_' + str(pattern))
+            class_file_list = os.listdir(result_dir_path)
+            print(result_dir_path, class_file_list)
 
         else:
             # User Image
             user_img = request.files['user_img']
-            user_img_path = './images/result_img/Band_dot/406.jpg'
+            user_dir_path = './images/result_img/Band_dot/'
+            search_file_list = glob.glob(user_dir_path)
 
-        return render_template('clothes.html', class_res=result_img_path, search_res=user_img_path)
+        return render_template('clothes.html', class_res=class_file_list, search_res=search_file_list)
 
 
 if __name__ == "__main__":
