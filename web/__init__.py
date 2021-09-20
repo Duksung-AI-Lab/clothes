33b4af795cd165137afd714fc547d28ee172dc89
kri628 <00a3d2@duksung.ac.kr>
2021-09-20 18:20:03 +0900

    pattern model predict 구현

--- web/__init__.py
+++ web/__init__.py
@@ -2,23 +2,41 @@ import glob
 import os, sys
 import random
 
+import cv2
+
 real_path = os.path.dirname(os.path.realpath(__file__))
 sub_path = os.path.split(real_path)[0]
 os.chdir(sub_path)
 
 from flask import Flask, escape, request, Response, g, make_response
 from flask.templating import render_template
-from werkzeug.utils import secure_filename
+
+from tensorflow.keras.models import load_model
+
+class_file_list, search_file_list = None, None
+os.chdir('web/static')
+
+
+def model_predict():
+    model = load_model('models/pattern_5class.h5')
+
+    original_image = cv2.imread('user_img.jpg', cv2.IMREAD_COLOR)
+    image = cv2.resize(original_image, (224, 224))
+    image = image.astype('float32') / 255.
+    image = image.reshape((1, 224, 224, 3))
+
+    pattern_cls_index = ['check', 'dot', 'floral', 'solid', 'stripe']
+    pattern_result_classes = model.predict_classes(image)
+    # pattern_result = model.predict(image)
+    # print('예측:', pattern_cls_index[pattern_result_classes[0]], 100 * max(pattern_result[0]))
+
+    os.remove('user_img.jpg')
+
+    return pattern_cls_index[pattern_result_classes[0]]
+
 
 app = Flask(__name__)
 app.debug = True
-
-
-def root_path():
-    # root 경로 유지
-    real_path = os.path.dirname(os.path.realpath(__file__))
-    sub_path = "\\".join(real_path.split("\\")[:-1])
-    return os.chdir(sub_path)
 
 
 # Main page
@@ -27,14 +45,11 @@ def clothes():
     return render_template('clothes.html')
 
 
-class_file_list, search_file_list = None, None
-os.chdir('web/static')
-
 @app.route('/', methods=['GET', 'POST'])
 def post():
     if request.method == 'POST':
-        # root_path()
         global class_file_list, search_file_list
+
         if "classform" in request.form:
             # Reference Image
             collar = str(request.form['collars'])
@@ -51,7 +66,11 @@ def post():
         else:
             # User Image
             user_img = request.files['user_img']
-            user_dir_path = './images/result_img/Band_dot/*'
+            user_img.save('user_img.jpg')
+
+            pattern = model_predict()
+
+            user_dir_path = os.path.join('images/result_img', 'Band' + '_' + pattern + '/*')
             search_file_list = glob.glob(user_dir_path)
 
             random.shuffle(search_file_list)
