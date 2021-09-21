aec44b562e1e10a8ac90b8667c409da0fa48b294
kri628 <00a3d2@duksung.ac.kr>
2021-09-21 15:02:36 +0900

    densenet 파일 수정

--- model_codes/transfer/densenet.py
+++ model_codes/transfer/densenet.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras import models, optimizers, Model
+from tensorflow.keras import models, optimizers, Model, Input, losses
 from tensorflow.keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.applications import densenet
 
@@ -14,8 +14,7 @@ np.random.seed(0)
 tf.set_random_seed(0)
 
 data_path = 'dataset/collar/collars_500x600/'
-
-image_size = (125, 150)
+image_size = (150, 150)
 
 train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    horizontal_flip=True,
@@ -38,14 +37,23 @@ test_generator = val_datagen.flow_from_directory(os.path.join(data_path, 'test')
 transfer_model = densenet.DenseNet121(weights='imagenet', include_top=False, input_shape=image_size + (3,))
 
 transfer_model.trainable = False
-model = models.Sequential()
-model.add(transfer_model)
-model.add(GlobalAveragePooling2D())
-model.add(Dense(64, activation='relu'))
-# model.add(Dropout(0.2))
-model.add(Dense(3, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam(),
+inputs = Input(shape=(150, 150, 3))
+# We make sure that the base_model is running in inference mode here,
+# by passing `training=False`. This is important for fine-tuning, as you will
+# learn in a few paragraphs.
+x = transfer_model(inputs, training=False)
+# Convert features of shape `base_model.output_shape[1:]` to vectors
+x = GlobalAveragePooling2D()(x)
+# A Dense classifier with a single unit (binary classification)
+outputs = Dense(3)(x)
+model = Model(inputs, outputs)
+
+# model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
+#               metrics=["accuracy"])
+
+model.compile(optimizer=optimizers.Adam(),
+              loss=losses.BinaryCrossentropy(from_logits=True),
               metrics=['acc'])
 
 epochs = 20
